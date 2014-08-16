cf8c5c647946d45f55c1ec48f963838eef8c2d4c
Martin Wernstål <m4rw3r@gmail.com>
2014-08-16 22:42:29 +0200

    Scan: Added verbose error message

--- sql.go
+++ sql.go
@@ -2,7 +2,17 @@ package uuid
 
 import (
 	"database/sql/driver"
+	"fmt"
+	"reflect"
 )
+
+type ErrInvalidType struct {
+	Type reflect.Type
+}
+
+func (e *ErrInvalidType) Error() string {
+	return fmt.Sprintf("uuid Scan(): invalid type '%s', expected string.", e.Type.String())
+}
 
 // Scan scans a uuid from the given interface instance and stores it
 func (u *UUID) Scan(val interface{}) error {
@@ -13,7 +23,7 @@ func (u *UUID) Scan(val interface{}) error {
 		}
 	}
 
-	return ErrInvalid
+	return &ErrInvalidType{reflect.TypeOf(val)}
 }
 
 // Value gives the database driver representation of the UUID
