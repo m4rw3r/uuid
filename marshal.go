264e945e63d8c16ab324d0eda817f955ddc2121b
Martin Wernstål <m4rw3r@gmail.com>
2014-08-19 22:02:13 +0200

    Inlined UUID.String() in MarshalText() and MarshalJSON()

--- marshal.go
+++ marshal.go
@@ -1,17 +1,68 @@
 package uuid
 
-import (
-	"fmt"
-)
-
-// MarshalText returns the string-representation of the UUID.
+// MarshalText returns the string-representation of the UUID as a byte-array.
 func (u UUID) MarshalText() ([]byte, error) {
-	return []byte(u.String()), nil
+	/* Inlined UUID.String() implementation, cannot reuse the one from
+	   UUID.String() as that cast will force an additional memory
+	   allocation because current version the compiler (go 1.3.1) cannot
+	   realize that the data pointer of the result of the UUID.String() call
+	   can be moved to the byte-slice return. Speed difference from inlining
+	   is about +20% */
+	/* Equivalent code:
+	   return []byte(u.String()), nil */
+	/* NOTE: If this is changed, also look at the following methods:
+	   UUID.MarshalJSON() and UUID.String() */
+	b := [36]byte{}
+
+	for i, n := range []int{
+		0, 2, 4, 6,
+		9, 11,
+		14, 16,
+		19, 21,
+		24, 26, 28, 30, 32, 34,
+	} {
+		b[n] = halfbyte2hexchar[(u[i]>>4)&0x0f]
+		b[n+1] = halfbyte2hexchar[u[i]&0x0f]
+	}
+
+	b[8] = '-'
+	b[13] = '-'
+	b[18] = '-'
+	b[23] = '-'
+
+	return b[:], nil
 }
 
 // MarshalJSON returns the string-representation of the UUID as a JSON-string.
 func (u UUID) MarshalJSON() ([]byte, error) {
-	return []byte(fmt.Sprintf("\"%8.x-%4.x-%4.x-%4.x-%12.x\"", u[0:4], u[4:6], u[6:8], u[8:10], u[10:16])), nil
+	/* Needs a slightly different code yet inlined to prevent extra memory
+	   allocations, it needs to be 38 bytes and the indices are shifted once
+	   to the right to make room for quotation marks */
+	/* Equivalent code:
+	   return []byte("\"" + u.String() + "\""), nil */
+	/* NOTE: If this is changed, also look at the following methods:
+	   UUID.MarshalText() and UUID.String() */
+	b := [38]byte{}
+
+	for i, n := range []int{
+		1, 3, 5, 7,
+		10, 12,
+		15, 17,
+		20, 22,
+		25, 27, 29, 31, 33, 35,
+	} {
+		b[n] = halfbyte2hexchar[(u[i]>>4)&0x0f]
+		b[n+1] = halfbyte2hexchar[u[i]&0x0f]
+	}
+
+	b[0] = '"'
+	b[9] = '-'
+	b[14] = '-'
+	b[19] = '-'
+	b[24] = '-'
+	b[37] = '"'
+
+	return b[:], nil
 }
 
 // UnmarshalText reads an UUID from a string into the UUID instance.
