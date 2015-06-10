1da426a10843d2f6f16d96cf6d16916a7f10d00b
Martin Wernstål <m4rw3r@gmail.com>
2015-06-10 16:41:15 +0200

    Tests: Refactored basic tests to use the same list of UUIDs for all parse-methods

--- uuid_test.go
+++ uuid_test.go
@@ -1,13 +1,117 @@
 package uuid
 
 import (
+	"fmt"
 	"testing"
 )
 
 var (
 	testStringUUID = "a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11"
+	testZeroString = "00000000-0000-0000-0000-000000000000"
 	testByteUUID   = []byte("a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11")
+	testMixed      = map[string]validator{
+		"00000000-0000-0000-0000-00000000000f": valid{"00000000-0000-0000-0000-00000000000f"},
+		"00000000-0000-0000-0000-0000000000f0": valid{"00000000-0000-0000-0000-0000000000f0"},
+		"00000000-0000-0000-0000-100000000000": valid{"00000000-0000-0000-0000-100000000000"},
+		"00000000-0000-0000-f000-000000000000": valid{"00000000-0000-0000-f000-000000000000"},
+		"00000000-0000-f000-0000-000000000000": valid{"00000000-0000-f000-0000-000000000000"},
+		"00000000-f000-0000-0000-000000000000": valid{"00000000-f000-0000-0000-000000000000"},
+		"f0000000-0000-0000-0000-000000000000": valid{"f0000000-0000-0000-0000-000000000000"},
+		"0f000000-0000-0000-0000-000000000000": valid{"0f000000-0000-0000-0000-000000000000"},
+		"000f0000-0000-0000-0000-000000000000": valid{"000f0000-0000-0000-0000-000000000000"},
+		"00000000-00f0-0000-0000-000000000000": valid{"00000000-00f0-0000-0000-000000000000"},
+		"f0000000-f000-f000-f000-f00000000000": valid{"f0000000-f000-f000-f000-f00000000000"},
+		"12345678-9abc-deff-edcb-a98765432100": valid{"12345678-9abc-deff-edcb-a98765432100"},
+		"ffffffff-ffff-ffff-ffff-ffffffffffff": valid{"ffffffff-ffff-ffff-ffff-ffffffffffff"},
+		"12345678-1234-5678-1234-567812345678": valid{"12345678-1234-5678-1234-567812345678"},
+		"c56a4180-65aa-42ec-a945-5fd21dec0538": valid{"c56a4180-65aa-42ec-a945-5fd21dec0538"},
+		"00000000-0000-0000-0000-000000000000": valid{"00000000-0000-0000-0000-000000000000"},
+		"00000000-0000-0000-0000-00000000000":  invalid{},
+		"":        invalid{},
+		"0":       invalid{},
+		"abcdefg": invalid{},
+		"fffffff-ffff-ffff-ffff-fffffffffffff":   invalid{}, /* all - shifted one to the left */
+		"fffffffff-ffff-ffff-ffff-fffffffffff":   invalid{}, /* all - shifted one to the right */
+		"ffffffff-ffff-ffff-ffff-fffffffffff":    invalid{}, /* one char too short */
+		"ffffffff-ffff-ffff-ffff-fffffffffffff":  invalid{}, /* one char too long */
+		"ffffffff-ffff-ffff-ffff-ffffffffffffff": invalid{}, /* two char too long */
+		"ffffffff-ffff-ffff-ffff--fffffffffff":   invalid{}, /* one dash instead of char */
+		"C56A4180-65AA-42EC-A945-5FD21DEC":       invalid{},
+		"x56a4180-h5aa-42ec-a945-5fd21dec0538":   invalid{},
+		"null": invalid{},
+		"something invalid here":                  invalid{},
+		"A0EEBC99-9C0B-4EF8-BB6D-6BB9BD380A11":    valid{"a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11"},
+		"{a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11}":  valid{"a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11"},
+		"a0eebc999c0b4ef8bb6d6bb9bd380a11":        valid{"a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11"},
+		"a0ee-bc99-9c0b-4ef8-bb6d-6bb9-bd38-0a11": valid{"a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11"},
+		"{a0eebc99-9c0b4ef8-bb6d6bb9-bd380a11}":   valid{"a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11"},
+	}
 )
+
+// validator validates errors and UUIDs for tests
+type validator interface {
+	ReadError(error) error
+	Validate(UUID) error
+	ValidateEmpty(UUID) error
+}
+
+// valid is an expected valid UUID read.
+type valid struct {
+	Str string
+}
+
+// ReadError validates any errors which might arise when reading the UUID.
+func (v valid) ReadError(err error) error {
+	if err != nil {
+		return fmt.Errorf("expected '%s', got error: %s", v.Str, err.Error())
+	}
+
+	return nil
+}
+
+// Validate validates a UUID with the expectation that it should not validate
+// upon read failure.
+func (v valid) Validate(u UUID) error {
+	if v.Str != u.String() {
+		return fmt.Errorf("string representation '%s' does not match '%s'.", u.String(), v.Str)
+	}
+
+	return nil
+}
+
+// ValidateEmpty validates a UUID where an error condition must set the UUID
+// to zero.
+func (v valid) ValidateEmpty(u UUID) error {
+	return v.Validate(u)
+}
+
+// invalid is an expected failure to read a UUID.
+type invalid struct{}
+
+// ReadError validates any errors which might arise when reading the UUID.
+func (v invalid) ReadError(err error) error {
+	if err == nil {
+		return fmt.Errorf("expected error")
+	}
+
+	return nil
+}
+
+// Validate validates a UUID with the expectation that it should not validate
+// upon read failure.
+func (v invalid) Validate(u UUID) error {
+	return nil
+}
+
+// ValidateEmpty validates a UUID where an error condition must set the UUID
+// to zero.
+func (v invalid) ValidateEmpty(u UUID) error {
+	if u != [16]byte{} {
+		return fmt.Errorf("expected empty UUID, got '%s'", u.String())
+	}
+
+	return nil
+}
 
 func TestZero(t *testing.T) {
 	u := UUID{}
@@ -20,13 +124,24 @@ func TestZero(t *testing.T) {
 func TestZero2(t *testing.T) {
 	u := UUID{}
 
-	if u.String() != "00000000-0000-0000-0000-000000000000" {
+	if u.String() != testZeroString {
 		t.Error("Default zero value is not zero uuid")
 	}
 }
 
 func TestFromString(t *testing.T) {
-	u, err := FromString("00000000-0000-0000-0000-000000000000")
+	for i, v := range testMixed {
+		u, err := FromString(i)
+		if e := v.ReadError(err); e != nil {
+			t.Errorf("FromString(%s): %s", i, e.Error())
+		} else if e := v.Validate(u); e != nil {
+			t.Errorf("FromString(%s): %s", i, e.Error())
+		}
+	}
+}
+
+func TestFromStringZero(t *testing.T) {
+	u, err := FromString(testZeroString)
 	if err != nil {
 		t.Error("Failed to read zero-uuid")
 	}
@@ -35,144 +150,71 @@ func TestFromString(t *testing.T) {
 		t.Error("Zero-uuid from string does not yield zero-uuid")
 	}
 
-	if u.String() != "00000000-0000-0000-0000-000000000000" {
+	if u.String() != testZeroString {
 		t.Error("zero uuid is not zero uuid")
 	}
 }
 
-func TestFromStringMultiple(t *testing.T) {
-	list := []string{
-		"00000000-0000-0000-0000-00000000000f",
-		"00000000-0000-0000-0000-0000000000f0",
-		"00000000-0000-0000-0000-100000000000",
-		"00000000-0000-0000-f000-000000000000",
-		"00000000-0000-f000-0000-000000000000",
-		"00000000-f000-0000-0000-000000000000",
-		"f0000000-0000-0000-0000-000000000000",
-		"0f000000-0000-0000-0000-000000000000",
-		"000f0000-0000-0000-0000-000000000000",
-		"00000000-00f0-0000-0000-000000000000",
-		"f0000000-f000-f000-f000-f00000000000",
-		"12345678-9abc-deff-edcb-a98765432100",
-		"ffffffff-ffff-ffff-ffff-ffffffffffff",
-	}
+func TestReadBytes(t *testing.T) {
+	for i, v := range testMixed {
+		u := UUID{}
 
-	for _, i := range list {
-		u, err := FromString(i)
-		if err != nil {
-			t.Error("Failed to read uuid '" + i + "':" + err.Error())
-
-			continue
+		err := u.ReadBytes([]byte(i))
+		if e := v.ReadError(err); e != nil {
+			t.Errorf("FromString(%s): %s", i, e.Error())
+		} else if e := v.Validate(u); e != nil {
+			t.Errorf("FromString(%s): %s", i, e.Error())
 		}
+	}
+}
 
-		if u.String() != i {
-			t.Error("String representation of UUID '" + i + "' does not match, got '" + u.String() + "'.")
+func TestSetString(t *testing.T) {
+	for i, v := range testMixed {
+		u := UUID{}
+
+		err := u.SetString(i)
+		if e := v.ReadError(err); e != nil {
+			t.Errorf("SetString(%s): %s", i, e.Error())
+		} else if e := v.Validate(u); e != nil {
+			t.Errorf("SetString(%s): %s", i, e.Error())
 		}
 	}
 }
 
 func TestMaybeFromString(t *testing.T) {
-	list := map[string]string{
-		"00000000-0000-0000-0000-00000000000f": "00000000-0000-0000-0000-00000000000f",
-		"12345678-1234-5678-1234-567812345678": "12345678-1234-5678-1234-567812345678",
-		"c56a4180-65aa-42ec-a945-5fd21dec0538": "c56a4180-65aa-42ec-a945-5fd21dec0538",
-		"00000000-0000-0000-0000-00000000000":  "00000000-0000-0000-0000-000000000000",
-		"":                                     "00000000-0000-0000-0000-000000000000",
-		"C56A4180-65AA-42EC-A945-5FD21DEC":     "00000000-0000-0000-0000-000000000000",
-		"x56a4180-h5aa-42ec-a945-5fd21dec0538": "00000000-0000-0000-0000-000000000000",
-		"null":                                 "00000000-0000-0000-0000-000000000000",
-		"something invalid here":               "00000000-0000-0000-0000-000000000000",
-	}
-
-	for input, expected := range list {
-		u := MaybeFromString(input)
-		if u.String() != expected {
-			t.Errorf("MaybeFromString(%s) does not match %s, got %s", input, expected, u.String())
+	for i, v := range testMixed {
+		u := MaybeFromString(i)
+		if e := v.ValidateEmpty(u); e != nil {
+			t.Errorf("MaybeFromString(%s): %s", i, e.Error())
 		}
 	}
 }
 
 func TestMustFromString(t *testing.T) {
-	list := map[string]string{
-		"00000000-0000-0000-0000-00000000000f": "00000000-0000-0000-0000-00000000000f",
-		"12345678-1234-5678-1234-567812345678": "12345678-1234-5678-1234-567812345678",
-		"c56a4180-65aa-42ec-a945-5fd21dec0538": "c56a4180-65aa-42ec-a945-5fd21dec0538",
-		"00000000-0000-0000-0000-00000000000":  "", /* Means panic is expected */
-		"":                                     "",
-		"C56A4180-65AA-42EC-A945-5FD21DEC":     "",
-		"x56a4180-h5aa-42ec-a945-5fd21dec0538": "",
-		"null":                                 "",
-		"something invalid here":               "",
-	}
-
-	for input, expected := range list {
-		testMustFromString(t, input, expected)
+	for i, v := range testMixed {
+		testMustFromString(t, i, v)
 	}
 }
 
-func testMustFromString(t *testing.T, input string, expected string) {
+func testMustFromString(t *testing.T, i string, v validator) {
 	var u UUID
 
 	defer func() {
-		err := recover()
+		r := recover()
 
-		if expected == "" && err == nil {
-			t.Errorf("MustFromString(%s) expected panic, got %s", input, u.String())
+		err, ok := r.(error)
+		if !ok && r != nil {
+			panic(fmt.Sprintf("in test for MustString: got non-error: %+v", r))
 		}
 
-		if expected != "" && err != nil {
-			t.Errorf("MustFromString(%s) paniced, expected %s", input, expected)
+		if e := v.ReadError(err); e != nil {
+			t.Errorf("FromString(%s): %s", i, e.Error())
 		}
 	}()
 
-	u = MustFromString(input)
-	if expected != "" && u.String() != expected {
-		t.Errorf("MustFromString(%s) does not match %s, got %s", input, expected, u.String())
-	}
-}
-
-func TestAlternativeValid(t *testing.T) {
-	uuid := "a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11"
-	list := []string{
-		"A0EEBC99-9C0B-4EF8-BB6D-6BB9BD380A11",
-		"{a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11}",
-		"a0eebc999c0b4ef8bb6d6bb9bd380a11",
-		"a0ee-bc99-9c0b-4ef8-bb6d-6bb9-bd38-0a11",
-		"{a0eebc99-9c0b4ef8-bb6d6bb9-bd380a11}",
-	}
-
-	for _, i := range list {
-		u, err := FromString(i)
-		if err != nil {
-			t.Error("Failed to read uuid '" + i + "':" + err.Error())
-
-			continue
-		}
-
-		if u.String() != uuid {
-			t.Error("String representation of UUID '" + i + "' does not match, got '" + u.String() + "'.")
-		}
-	}
-}
-
-func TestInvalid(t *testing.T) {
-	list := []string{
-		"",
-		"0",
-		"abcdefg",
-		"fffffff-ffff-ffff-ffff-fffffffffffff",   /* all - shifted one to the left */
-		"fffffffff-ffff-ffff-ffff-fffffffffff",   /* all - shifted one to the right */
-		"ffffffff-ffff-ffff-ffff-fffffffffff",    /* one char too short */
-		"ffffffff-ffff-ffff-ffff-fffffffffffff",  /* one char too long */
-		"ffffffff-ffff-ffff-ffff-ffffffffffffff", /* two char too long */
-		"ffffffff-ffff-ffff-ffff--fffffffffff",   /* one dash instead of char */
-	}
-
-	for _, i := range list {
-		_, err := FromString(i)
-		if err == nil {
-			t.Error("Expected error but got nothing when reading uuid '" + i + "'.")
-		}
+	u = MustFromString(i)
+	if e := v.Validate(u); e != nil {
+		t.Errorf("MustFromString(%s): %s", i, e.Error())
 	}
 }
 
@@ -183,11 +225,11 @@ func TestV4(t *testing.T) {
 	}
 
 	if u[6]&0xf0 != 0x40 {
-		t.Error("UUID generated from V4() does not have the version byte set to 4: '" + u.String() + "'.")
+		t.Errorf("UUID generated from V4() does not have the version byte set to 4: '%s'.", u.String())
 	}
 
 	if u[8]&0xf0 < 0x80 || u[8]&0xf0 > 0xB0 {
-		t.Error("UUID generated from V4() does not have the 9th byte beginning with 8, 9, A or B: '" + u.String() + "'.")
+		t.Errorf("UUID generated from V4() does not have the 9th byte beginning with 8, 9, A or B: '%s'.", u.String())
 	}
 }
 
